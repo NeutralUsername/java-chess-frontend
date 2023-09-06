baacdb478dbb786fa0e846f4d2bc0801f131c6d7
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 14:21:32 +0200

    refactoring

--- PrimaryStage.java
+++ PrimaryStage.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,17 +9,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
 
-public class Client extends Application {
-    public Client () {
-        launch();
-    }
+public class PrimaryStage extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
+            Socket client = new Socket("localhost", 4711);
+            System.out.println("Client: connected to " +
+                    client.getInetAddress());
+            InputStream in = client.getInputStream();
+            byte b[] = new byte[100];
+            int bytes = in.read(b);
+            System.out.println("Client: recieved " + bytes + " Bytes from Server");
+            String message = new String(b);
+            System.out.println("Client: Message from Server: " +
+                    message);
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
