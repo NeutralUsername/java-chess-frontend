6f24b0a8198448131ac23afaffbeb2bc51449922
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 12:07:02 +0200

    basic networking

--- Client.java
+++ Client.java
@@ -5,9 +5,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
  
 public class Client extends Application {
     public static void main(String[] args) {
+        try {
+            Socket client = new Socket("localhost", 4711);
+            System.out.println("Client: connected to " +
+           client.getInetAddress());
+            InputStream in = client.getInputStream();
+            byte b[] = new byte[100];
+            int bytes = in.read(b);
+            System.out.println("Client: recieved " + bytes + " Bytes from Server");
+            String message = new String(b);
+            System.out.println("Client: Message from Server: " +
+           message);
+            } catch (IOException e) {
+           // Generelles Exception-Handling zu Demo-Zwecken
+            e.printStackTrace();
+            }
         launch(args);
     }
     
